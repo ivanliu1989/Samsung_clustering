7ac5e4865267b0bdd27c5360537b5605599c88e7
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-17 08:53:49 +1000

    plotting

--- Samsung_clustering.R
+++ Samsung_clustering.R
@@ -4,7 +4,14 @@ setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Samsung_clus
 clusterEx.zip <- "clusteringEx_data.zip"
 clusterEx.list <- unzip(clusterEx.zip)
 clusterEx.list
-faceData <- load(clusterEx.list[1])
-head(faceData)
-samsungData <- load(clusterEx.list[2])
-head(samsungData)
+load(clusterEx.list[2])
+names(samsungData)[1:12]
+table(samsungData$activity)
+
+## Plotting average acceleration for first subject
+par(mfcol = c(1,2),mar = c(5,4,1,1))
+samsungData <- transform(samsungData, activity=factor(activity))
+sub1 <- subset(samsungData,subject =1 )
+plot(sub1[,1], col=sub1$activity, ylab=names(sub1)[1])
+plot(sub1[,2], col=sub1$activity, ylab=names(sub1)[2])
+legend("bottomright", legend=unique(sub1$activity), col=unique(sub1$activity), pch=1)
