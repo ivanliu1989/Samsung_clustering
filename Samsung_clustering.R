7108b05383f6f44ff18e5185362c7f5bc9ab0379
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-17 08:58:30 +1000

    plotting

--- Samsung_clustering.R
+++ Samsung_clustering.R
@@ -9,9 +9,15 @@ names(samsungData)[1:12]
 table(samsungData$activity)
 
 ## Plotting average acceleration for first subject
+png("AvgAcc.png",height = 480,width = 960)
 par(mfcol = c(1,2),mar = c(5,4,1,1))
 samsungData <- transform(samsungData, activity=factor(activity))
 sub1 <- subset(samsungData,subject =1 )
 plot(sub1[,1], col=sub1$activity, ylab=names(sub1)[1])
 plot(sub1[,2], col=sub1$activity, ylab=names(sub1)[2])
 legend("bottomright", legend=unique(sub1$activity), col=unique(sub1$activity), pch=1)
+dev.off()
+
+## Clustering based just on average acceleration
+distanceMatrix <- dist(sub1[,1:3])
+hclustering  <- hclust(distanceMatrix)
